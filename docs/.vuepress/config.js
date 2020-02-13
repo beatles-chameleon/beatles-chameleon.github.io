5b4138f6d1b002f142a7bb45e8158caa0bb9f7a6
李延峰 <flex@didiglobal.com>
2020-02-14 00:31:17 +0800

    更新 VuePress 配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'cml',
+  description: '小程序跨端开发解决方案',
+  dest: './dist',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+    ],
+    sidebar: ['/', '/guide/'],
+    activeHeaderLinks: false,
+  },
+};
