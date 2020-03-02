ca9b038e65ead28e12ae00dec182f92597b8b445
李延峰 <flex@didiglobal.com>
2020-03-03 05:42:37 +0800

    更新文档配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,23 @@
+const path = require('path');
+
 module.exports = {
   title: 'CML',
-  description: '小程序跨端开发解决方案',
+  description: '小程序跨端开发统一解决方案',
   dest: './dist',
   themeConfig: {
+    logo: '/logo.svg',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
     ],
     sidebar: ['/', '/guide/'],
     activeHeaderLinks: false,
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname),
+      },
+    },
   },
 };
