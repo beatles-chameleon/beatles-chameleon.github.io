22f022a258c701b3ab663eefc4a84f03594ade9b
李延峰 <flex@didiglobal.com>
2020-03-04 07:33:18 +0800

    添加 favicon.ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,10 @@ const path = require('path');
 module.exports = {
   title: 'CML',
   description: '小程序跨端开发统一解决方案',
+  head: [['link', { rel: 'shortcut icon', type: 'image/x-icon', href: `/favicon.ico` }]],
   dest: './dist',
   themeConfig: {
-    logo: '/logo.svg',
+    logo: '/brand/logo.svg',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
