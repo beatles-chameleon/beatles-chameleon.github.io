f1939b9b655e67cedf902a497e11573944d7ea5e
李延峰 <flex@didiglobal.com>
2020-03-06 09:05:51 +0800

    优化首页 UI 与 CSS

--- docs/.vuepress/components/HomeView.vue
+++ docs/.vuepress/components/HomeView.vue
@@ -12,34 +12,34 @@
         <p class="desc">{{ description }}</p>
         <div class="links">
           <a class="btn btn-primary" href="/get-started.html">快速开始</a>
-          <a class="btn btn-default" :href="linkGitHub" target="_blank" rel="noopener">GitHub</a>
+          <a class="btn btn-default" :href="gitHubUrl" target="_blank" rel="noopener">GitHub</a>
         </div>
       </div>
     </section>
-    <section class="flows gray">
-      <div class="card ends">
+    <section class="cards gray">
+      <div class="card checklist">
         <h2>多端支持</h2>
         <ul>
-          <li v-for="item in ends">
+          <li v-for="item in apps">
             <img :src="item.icon" alt="icon" />
             <span>{{ item.name }}</span>
             <span class="icon-check"></span>
           </li>
         </ul>
       </div>
-      <div class="card standard">
+      <div class="card blocks">
         <h2>MVVM 统一标准</h2>
         <ul>
-          <li v-for="item in standard" :class="item.className">
+          <li v-for="item in mvvm" :class="item.className">
             <h3>{{ item.name }}</h3>
             <p>{{ item.desc }}</p>
           </li>
         </ul>
       </div>
-      <div class="card helpers">
+      <div class="card contents">
         <h2>渐进式接入</h2>
         <ul>
-          <li v-for="item in helpers">
+          <li v-for="item in run">
             <img :src="item.icon" alt="icon" />
             <div>
               <h3>{{ item.name }}</h3>
@@ -49,7 +49,7 @@
         </ul>
       </div>
     </section>
-    <section v-for="group in cycles" class="cycle" :class="group.className">
+    <section v-for="group in eco" class="bars" :class="group.className">
       <h2>{{ group.name }}</h2>
       <ul>
         <li v-for="item in group.list">
@@ -58,9 +58,9 @@
             <h3>{{ item.name }}</h3>
             <p>{{ item.desc }}</p>
           </div>
-          <span v-if="item.link" class="entry link">查看详情<i>→</i></span>
-          <span v-else-if="item.link !== undefined" class="entry disable">敬请期待</span>
-          <span v-else class="entry"></span>
+          <span v-if="item.link" class="active">查看详情<i>→</i></span>
+          <span v-else-if="item.link !== undefined" class="disable">敬请期待</span>
+          <span v-else class="none"></span>
           <template v-if="item.link">
             <a v-if="isExternal(item.link)" :href="item.link" target="_blank" rel="noopener"></a>
             <a v-else :href="item.link"></a>
@@ -68,17 +68,17 @@
         </li>
       </ul>
     </section>
-    <section class="uni">
+    <section class="grid">
       <h2>MVVM+ 跨端标准协议</h2>
       <ul>
-        <li v-for="item in uni">
+        <li v-for="item in sdk">
           <img :src="item.icon" alt="icon" />
           <h3>{{ item.name }}</h3>
           <p v-for="child in item.list">{{ child }}</p>
         </li>
       </ul>
     </section>
-    <section class="cases">
+    <section class="slider">
       <h2>应用案例</h2>
       <carousel
         :per-page-custom="[
@@ -95,9 +95,9 @@
         </slide>
       </carousel>
     </section>
-    <section class="actions gray">
+    <section class="entry gray">
       <a class="btn btn-primary" href="/get-started.html">快速开始</a>
-      <a class="btn btn-default" :href="linkGitHub" target="_blank" rel="noopener">GitHub</a>
+      <a class="btn btn-default" :href="gitHubUrl" target="_blank" rel="noopener">GitHub</a>
     </section>
     <footer class="world">
       <div class="left">
@@ -138,54 +138,55 @@
 import { Carousel, Slide } from 'vue-carousel';
 import config from '@/config';
 // Brand
-import logo from '@/public/brand/logo.svg';
-import logoDark from '@/public/brand/logo_dark.svg';
+import logo from '@/public/logo.svg';
+import logoDark from '@/public/logo_dark.svg';
 // 多端支持
-import iconEndH5 from '@/public/images/icon_end_h5.png';
-import iconEndHybrid from '@/public/images/icon_end_hybrid.png';
-import iconEndWeixin from '@/public/images/icon_end_weixin.png';
-import iconEndAlipay from '@/public/images/icon_end_alipay.png';
-import iconEndBaidu from '@/public/images/icon_end_baidu.png';
-import iconEndQQ from '@/public/images/icon_end_qq.png';
-import iconEndByteDance from '@/public/images/icon_end_bytedance.png';
-import iconEndQuickApp from '@/public/images/icon_end_quickapp.png';
+import appH5 from '@/public/app_h5.png';
+import appHybrid from '@/public/app_hybrid.png';
+import appWeixin from '@/public/app_weixin.png';
+import appAlipay from '@/public/app_alipay.png';
+import appBaidu from '@/public/app_baidu.png';
+import appQQ from '@/public/app_qq.png';
+import appByteDance from '@/public/app_bytedance.png';
+import appQuickApp from '@/public/app_quickapp.png';
 // 渐进式接入
-import iconHelperIO from '@/public/images/icon_helper_io.svg';
-import iconHelperEasy from '@/public/images/icon_helper_easy.svg';
-// 组件生态圈 & // 全方位能力供应
-import iconUIBase from '@/public/images/icon_ui_base.svg';
-import iconUICML from '@/public/images/icon_ui_cml.svg';
-import iconUILight from '@/public/images/icon_ui_light.svg';
-import iconEcoPlugin from '@/public/images/icon_eco_plugin.svg';
-import iconEcoLinter from '@/public/images/icon_eco_linter.svg';
-import iconEcoGitHub from '@/public/images/icon_eco_github.svg';
+import runShift from '@/public/run_shift.svg';
+import runhelper from '@/public/run_helper.svg';
+// 组件生态圈 & 全方位能力供应
+import ecoBase from '@/public/eco_base.svg';
+import ecoCML from '@/public/eco_cml.svg';
+import ecoLight from '@/public/eco_light.svg';
+import ecoPlugin from '@/public/eco_plugin.svg';
+import ecoLinter from '@/public/eco_linter.svg';
+import ecoGitHub from '@/public/eco_github.svg';
 // MVVM+ 跨端标准协议
-import iconUniDev from '@/public/images/icon_uni_dev.svg';
-import iconUniPut from '@/public/images/icon_uni_put.svg';
-import iconUniLink from '@/public/images/icon_uni_link.svg';
-import iconUniPlug from '@/public/images/icon_uni_plug.svg';
-import iconUniLib from '@/public/images/icon_uni_lib.svg';
-import iconUniOne from '@/public/images/icon_uni_one.svg';
+import sdkDev from '@/public/sdk_dev.svg';
+import sdkPut from '@/public/sdk_put.svg';
+import sdkLink from '@/public/sdk_link.svg';
+import sdkPlug from '@/public/sdk_plug.svg';
+import sdkLib from '@/public/sdk_lib.svg';
+import sdkUni from '@/public/sdk_uni.svg';
 // 应用案例
-import imgCase1 from '@/public/images/case_1.png';
-import imgCase2 from '@/public/images/case_2.png';
-import imgCase3 from '@/public/images/case_3.png';
-import imgCase4 from '@/public/images/case_4.png';
-import imgCase5 from '@/public/images/case_5.png';
-import imgCase6 from '@/public/images/case_6.png';
+import case1 from '@/public/case_1.png';
+import case2 from '@/public/case_2.png';
+import case3 from '@/public/case_3.png';
+import case4 from '@/public/case_4.png';
+import case5 from '@/public/case_5.png';
+import case6 from '@/public/case_6.png';
 // Footer
-import imgQrCodeWeixin from '@/public/images/qrCodeWeixin.png';
-import imgQrCodeQQ from '@/public/images/qrCodeQQ.jpg';
-import imgQrCodeDeer from '@/public/images/qrCodeDeer.png';
+import qrWeixin from '@/public/qr_weixin.png';
+import qrQQ from '@/public/qr_qq.jpg';
+import qrDeer from '@/public/qr_deer.png';
 
+// 信息
 const { title, description } = config;
 const nowYear = new Date().getFullYear();
 // 链接
-const linkGitHub = 'https://github.com/didi/chameleon';
-const linkTeam = 'https://github.com/didi/chameleon/wiki/Chameleon%E5%9B%A2%E9%98%9F';
-const linkJoinUs = 'https://github.com/didi/chameleon/wiki/%E5%8A%A0%E5%85%A5%E6%88%91%E4%BB%AC';
-const linkContact = 'mailto:CML@didichuxing.com';
-const linkCoders = 'https://github.com/didi/chameleon/wiki/Contributors';
+const gitHubUrl = 'https://github.com/didi/chameleon';
+const teamUrl = 'https://github.com/didi/chameleon/wiki/Chameleon%E5%9B%A2%E9%98%9F';
+const joinUsUrl = 'https://github.com/didi/chameleon/wiki/%E5%8A%A0%E5%85%A5%E6%88%91%E4%BB%AC';
+const emailUrl = 'mailto:CML@didichuxing.com';
+const codersUrl = 'https://github.com/didi/chameleon/wiki/Contributors';
 
 export default {
   components: {
@@ -200,40 +201,40 @@ export default {
       title,
       description,
       // Footer
-      linkGitHub,
+      gitHubUrl,
       copyright: `Copyright © ${nowYear} DiDi Chuxing Inc.`,
       // 多端支持
-      ends: [
-        { name: 'Web 端', icon: iconEndH5 },
-        { name: 'Hybrid (iOS, Android)', icon: iconEndHybrid },
-        { name: '微信小程序', icon: iconEndWeixin },
-        { name: '支付宝小程序', icon: iconEndAlipay },
-        { name: '百度小程序', icon: iconEndBaidu },
-        { name: 'QQ 小程序', icon: iconEndQQ },
-        { name: '字节跳动小程序', icon: iconEndByteDance },
-        { name: '快应用', icon: iconEndQuickApp },
+      apps: [
+        { name: 'Web 端', icon: appH5 },
+        { name: 'Hybrid (iOS, Android)', icon: appHybrid },
+        { name: '微信小程序', icon: appWeixin },
+        { name: '支付宝小程序', icon: appAlipay },
+        { name: '百度小程序', icon: appBaidu },
+        { name: 'QQ 小程序', icon: appQQ },
+        { name: '字节跳动小程序', icon: appByteDance },
+        { name: '快应用', icon: appQuickApp },
       ],
       // MVVM 统一标准
-      standard: [
+      mvvm: [
         { name: 'Model', desc: '标准 JS 语法，标准系统接口', className: 'model' },
         { name: 'View', desc: '标准 Render Engine', className: 'view' },
         { name: 'View Model', desc: '标准模版 DSL', className: 'view-model' },
       ],
       // 渐进式接入
-      helpers: [
+      run: [
         {
           name: '组件导入与导出',
           list: ['支持导入各端原生组件', 'CML 组件导出至各端'],
-          icon: iconHelperIO,
+          icon: runShift,
         },
         {
           name: 'easy-chameleon',
           list: ['优雅升级 CML 跨端方案', '使用方便，简洁高效'],
-          icon: iconHelperEasy,
+          icon: runhelper,
         },
       ],
       // 组件生态圈 & 全方位能力供应
-      cycles: [
+      eco: [
         {
           name: '组件生态圈',
           list: [
@@ -241,19 +242,19 @@ export default {
               name: 'chameleon-ui-builtin',
               desc: '内置基础组件，多端适配性强',
               link: 'base-ui.html',
-              icon: iconUIBase,
+              icon: ecoBase,
             },
             {
               name: 'CML UI',
               desc: '丰富基础组件，支持按需引用',
               link: 'cml-ui.html',
-              icon: iconUICML,
+              icon: ecoCML,
             },
             {
               name: 'Light UI',
               desc: '配置灵活多样，可定制化程度高',
               link: '',
-              icon: iconUILight,
+              icon: ecoLight,
             },
           ],
         },
@@ -264,57 +265,57 @@ export default {
             {
               name: '编辑器插件',
               desc: 'VS Code、WebStorm、Sublime、Atom 插件，语法高亮、指令补全',
-              icon: iconEcoPlugin,
+              icon: ecoPlugin,
             },
             {
               name: 'Chameleon Linter',
               desc: '多端规范检查，语法错误提示，运行时多态检查',
-              icon: iconEcoLinter,
+              icon: ecoLinter,
             },
             {
               name: 'GitHub 社区',
               desc: '7000+ Star，氛围活跃，有问必答',
-              link: linkGitHub,
-              icon: iconEcoGitHub,
+              link: gitHubUrl,
+              icon: ecoGitHub,
             },
           ],
         },
       ],
       // MVVM+ 跨端标准协议
-      uni: [
+      sdk: [
         {
           name: '开发效率高',
           list: ['独创强大的跨多端语法检查功能', '拥有业内最优秀工程化设计'],
-          icon: iconUniDev,
+          icon: sdkDev,
         },
         {
           name: '可维护性好',
           list: ['独创多态协议', '轻松维护一套代码实现跨多端'],
-          icon: iconUniPut,
+          icon: sdkPut,
         },
         {
           name: '渐进式接入',
           list: ['一键导出原生组件', '无污染引用原生组件'],
-          icon: iconUniLink,
+          icon: sdkLink,
         },
         {
           name: '扩展性强',
           list: ['基于多态协议，可扩展任意底层接口', '不强依赖框架的更新'],
-          icon: iconUniPlug,
+          icon: sdkPlug,
         },
         {
           name: '基础库丰富',
           list: ['统一的 CML Native SDK', '丰富的组件、API 库'],
-          icon: iconUniLib,
+          icon: sdkLib,
         },
         {
           name: '多端高度一致',
           list: ['统一的代码、界面交互、开发流程', '多层次高度统一'],
-          icon: iconUniOne,
+          icon: sdkUni,
         },
       ],
       // 应用案例
-      cases: [imgCase1, imgCase2, imgCase3, imgCase4, imgCase5, imgCase6],
+      cases: [case1, case2, case3, case4, case5, case6],
       // Footer
       world: [
         {
@@ -327,19 +328,19 @@ export default {
         {
           name: '社区',
           list: [
-            { name: 'GitHub', link: linkGitHub },
-            { name: '微信交流群', img: imgQrCodeWeixin },
-            { name: 'QQ 交流群', img: imgQrCodeQQ },
-            { name: '顺风公鹿', img: imgQrCodeDeer },
+            { name: 'GitHub', link: gitHubUrl },
+            { name: '微信交流群', img: qrWeixin },
+            { name: 'QQ 交流群', img: qrQQ },
+            { name: '顺风公鹿', img: qrDeer },
           ],
         },
         {
           name: '关于',
           list: [
-            { name: 'CML 团队', link: linkTeam },
-            { name: '加入我们', link: linkJoinUs },
-            { name: '联系我们', link: linkContact },
-            { name: '贡献者', link: linkCoders },
+            { name: 'CML 团队', link: teamUrl },
+            { name: '加入我们', link: joinUsUrl },
+            { name: '联系我们', link: emailUrl },
+            { name: '贡献者', link: codersUrl },
           ],
         },
       ],
