f1adfbd35934912b6ad318195e2056b1552a4b5a
李延峰 <flex@didiglobal.com>
2020-03-12 00:19:27 +0800

    重新设计 404 页面

---
+++ site/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,38 @@
+<template>
+  <DefaultLayout>
+    <template #page-top>
+      <div class="not-found">
+        <h1>404</h1>
+        <p>"{{ getMsg() }}"</p>
+        <RouterLink to="/">回到首页</RouterLink>
+      </div>
+    </template>
+    <template #page-bottom>
+      <Footer />
+    </template>
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from '@parent-theme/layouts/Layout.vue';
+import Footer from '../components/Footer.vue';
+
+const msgs = [
+  '这里好像什么都没有',
+  '我们怎么到这儿的？',
+  '这里是四〇四研究所',
+  '看来我们的链接不太对',
+];
+
+export default {
+  components: {
+    DefaultLayout,
+    Footer,
+  },
+  methods: {
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    },
+  },
+};
+</script>
