66047586081d02e951319a38451cf206cf578e56
李延峰 <flex@didiglobal.com>
2020-03-13 03:17:22 +0800

    更新链接 slug 与导航排列

--- site/.vuepress/enhanceApp.js
+++ site/.vuepress/enhanceApp.js
@@ -2,6 +2,6 @@ export default ({ router }) => {
   router.addRoutes([
     { path: '/docs/', redirect: '/docs/introduction' },
     { path: '/components/', redirect: '/components/base' },
-    { path: '/tutorial/', redirect: '/tutorial/cml-app' },
+    { path: '/tutorial/', redirect: '/tutorial/first-cml-app' },
   ]);
 };
